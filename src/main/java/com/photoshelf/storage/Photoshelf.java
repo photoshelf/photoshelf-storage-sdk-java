5f0eabc83dcaa5cd815ff314c65ef76bff8c0f33
shunsuke maeda <duck8823@gmail.com>
2017-07-06 23:17:03 +0900

    Fix Content-Type in POST/PUT method

--- src/main/java/com/photoshelf/storage/Photoshelf.java
+++ src/main/java/com/photoshelf/storage/Photoshelf.java
@@ -8,6 +8,7 @@ import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.HttpClients;
 
@@ -42,11 +43,13 @@ public class Photoshelf {
 
 	public String create(byte[] photo) throws IOException {
 		HttpPost request = new HttpPost(this.url + "/");
-		request.setEntity(MultipartEntityBuilder.create().addBinaryBody("photo", photo).build());
+		request.setEntity(MultipartEntityBuilder.create()
+				.addBinaryBody("photo", photo, ContentType.create("application/octet-stream"), "image")
+				.build());
 		HttpResponse response = this.httpClient.execute(request);
 		if (response.getStatusLine().getStatusCode() == SC_CREATED) {
 			JsonObject json = new Gson().fromJson(new InputStreamReader(response.getEntity().getContent()), JsonObject.class);
-			return json.get("id").getAsString();
+			return json.get("Id").getAsString();
 		} else {
 			throw new IllegalStateException(response.toString());
 		}
@@ -54,7 +57,9 @@ public class Photoshelf {
 
 	public void replace(String id, byte[] photo) throws IOException {
 		HttpPut request = new HttpPut(this.url + "/" + id);
-		request.setEntity(MultipartEntityBuilder.create().addBinaryBody("photo", photo).build());
+		request.setEntity(MultipartEntityBuilder.create()
+				.addBinaryBody("photo", photo, ContentType.create("application/octet-stream"), "image")
+				.build());
 		HttpResponse response = this.httpClient.execute(request);
 		if (response.getStatusLine().getStatusCode() != SC_OK) {
 			throw new IllegalStateException(response.toString());
