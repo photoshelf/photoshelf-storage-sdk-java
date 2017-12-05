0ea9b8c0a0dcda22f1e1ceceea86b1a9e5fb3005
shunsuke maeda <duck8823@gmail.com>
2017-12-06 08:16:59 +0900

    Modularization

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module photoshelf.storage {
+    requires gson;
+    requires httpcore;
+    requires httpclient;
+    requires httpmime;
+    requires java.activation;
+    exports com.photoshelf.storage;
+}
